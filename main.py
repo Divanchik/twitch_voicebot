a6b2c083887c4c9a2c4b8058b838f30462fe7dc7
Divanchik <d-sh-2016@list.ru>
2023-05-11 04:07:53 +0400

    parsing rework and refactoring

--- main.py
+++ main.py
@@ -5,155 +5,35 @@ import json
 import re
 import os
 from twitch_functions import validate_token, refresh_token
+from twitch_message import *
 CONFIG_PATH = "./config.json"
-ONLY_HIGHLIGHTED = True
+ONLY_HIGHLIGHTED = False
+VOICE = True
 
-
-def parse_tags(s: str):
-    tags_to_ignore = {
-        'client-nonce': None,
-        'flags': None
-    }
-
-    parsed_tags = dict()
-    tags = s.split(";")
-    for tag in tags:
-        parsed_tag = tag.split("=")
-        tag_value = None if parsed_tag[1] == '' else parsed_tag[1]
-        if parsed_tag[0] in ["badges", "badge-info"]:
-            if tag_value is not None:
-                tmp = dict()
-                badges = tag_value.split(',')
-                for pair in badges:
-                    badge_parts = pair.split('/')
-                    tmp[badge_parts[0]] = badge_parts[1]
-                parsed_tags[parsed_tag[0]] = tmp.copy()
-            else:
-                parsed_tags[parsed_tag[0]] = None
-        elif parsed_tag[0] == 'emotes':
-            if tag_value is not None:
-                emotes = dict()
-                emotes_list = tag_value.split('/')
-                for emote in emotes_list:
-                    emote_parts = emote.split(':')
-                    text_pos = []
-                    positions = emote_parts[1].split(',')
-                    for pos in positions:
-                        pos_parts = pos.split('-')
-                        text_pos.append({
-                            'start_pos': pos_parts[0],
-                            'end_pos': pos_parts[1]
-                        })
-                    emotes[emote_parts[0]] = text_pos.copy()
-                parsed_tags[parsed_tag[0]] = emotes.copy()
-            else:
-                parsed_tags[parsed_tag[0]] = None
-        elif parsed_tag[0] == 'emote-sets':
-            emote_set_ids = tag_value.split(',')
-            parsed_tags[parsed_tag[0]] = emote_set_ids
-        else:
-            if parsed_tag[0] in tags_to_ignore.keys():
-                pass
-            else:
-                parsed_tags[parsed_tag[0]] = tag_value
-    return parsed_tags
-        
-
-def parse_cmd(s: str):
-    return None
-
-
-def parce_src(s: str):
-    src_parts = s.split("!")
-    return {
-        "nick": src_parts[0] if len(src_parts) == 2 else None,
-        "host": src_parts[1] if len(src_parts) == 2 else src_parts[0]
-    }
-
-
-def parse_params(s: str):
-    res = {}
-    idx = 0
-    command_parts = s[idx+1:].strip()
-    params_idx = command_parts.find(" ")
-
-    if params_idx == -1:
-        res["bot_command"] = command_parts[:]
-    else:
-        res["bot_command"] = command_parts[:params_idx]
-        res["bot_command_params"] = command_parts[params_idx:].strip()
-    return res
-
-
-def parse_msg(msg: str):
-    logging.info(f"parsing ({msg})")
-    parsed_message = {
-        "tags": None,
-        "source": None,
-        "command": None,
-        "parameters": None
-    }
-
-    idx = 0
-
-    raw_tags = ""
-    raw_source = ""
-    raw_command = ""
-    raw_parameters = ""
-
-    # get tags component
-    if msg[idx] == "@":
-        end_idx = msg.index(" ")
-        raw_tags = msg[1:end_idx]
-        idx = end_idx + 1
     
-    # get source component
-    if msg[idx] == ":":
-        idx += 1
-        end_idx = msg.index(" ", idx)
-        raw_source = msg[idx:end_idx]
-        idx = end_idx + 1
-    
-    # get command component
-    end_idx = msg.find(":", idx)
-    if end_idx == -1:
-        end_idx = len(msg)
-    raw_command = msg[idx:end_idx].strip()
-
-    # get parameters component
-    if end_idx != len(msg):
-        idx = end_idx + 1
-        raw_parameters = msg[idx:]
-    
-    # parse
-    parsed_message["command"] = raw_command
-    if len(raw_tags) != 0:
-        parsed_message["tags"] = parse_tags(raw_tags)
-    parsed_message["source"] = parce_src(raw_source)
-    parsed_message["parameters"] = raw_parameters
-    # if len(raw_parameters) != 0 and raw_parameters[0] == "!":
-    #     parsed_message["command"] = parse_params(raw_parameters)
-    
-    return parsed_message
-    
-
 async def handle_message(wsock, message):
-    global ONLY_HIGHLIGHTED
+    global ONLY_HIGHLIGHTED, VOICE
+    nicks_to_ignore = ["moobot"]
+    if len(message) == 0:
+        return
     if re.match("PING.*", message):
         await wsock.send(f"PONG{message[4:]}")
         logging.info("PING PONG")
     else:
-        msg = parse_msg(message)
-        if (ONLY_HIGHLIGHTED and msg['tags'].get('msg-id') == 'highlighted-message') or (not ONLY_HIGHLIGHTED):
-            os.system(f"echo \"{msg['parameters']}\" | RHVoice-test -p Anna -r 80")
-        logging.info(f"{msg['source'].get('nick')} >> {msg['parameters']}")
+        msg = parse_message(message)
+        if msg['command'] is not None and msg['command'].get('command') == 'PRIVMSG':
+            logging.info(f"{msg['source'].get('nick')} >> {msg['parameters']}")
+            if VOICE and ((not ONLY_HIGHLIGHTED) or (ONLY_HIGHLIGHTED and msg['tags'] is not None and msg['tags'].get('msg-id') == 'highlighted-message')):
+                os.system(f"echo \"{msg['parameters']}\" | RHVoice-test -p Anna -r 80")
         await asyncio.sleep(0.5)
 
 
 async def listener(wsock, flogger: logging.Logger):
-    async for message in wsock:
-        flogger.info(message)
-        await handle_message(wsock, message)
+    async for raw_input in wsock:
+        flogger.info(raw_input)
+        messages = raw_input.split('\n')
+        for msg in messages:
+            await handle_message(wsock, msg)
 
 
 async def main(config, flogger: logging.Logger):
---
+++ twitch_message.py
@@ -0,0 +1,139 @@
+def parse_tags(s: str):
+    tags_to_ignore = {
+        'client-nonce': None,
+        'flags': None
+    }
+
+    parsed_tags = dict()
+    tags = s.split(";")
+    for tag in tags:
+        parsed_tag = tag.split("=")
+        tag_value = None if parsed_tag[1] == '' else parsed_tag[1]
+        if parsed_tag[0] in ["badges", "badge-info"]:
+            if tag_value is not None:
+                tmp = dict()
+                badges = tag_value.split(',')
+                for pair in badges:
+                    badge_parts = pair.split('/')
+                    tmp[badge_parts[0]] = badge_parts[1]
+                parsed_tags[parsed_tag[0]] = tmp.copy()
+            else:
+                parsed_tags[parsed_tag[0]] = None
+        elif parsed_tag[0] == 'emotes':
+            if tag_value is not None:
+                emotes = dict()
+                emotes_list = tag_value.split('/')
+                for emote in emotes_list:
+                    emote_parts = emote.split(':')
+                    text_pos = []
+                    positions = emote_parts[1].split(',')
+                    for pos in positions:
+                        pos_parts = pos.split('-')
+                        text_pos.append({
+                            'start_pos': pos_parts[0],
+                            'end_pos': pos_parts[1]
+                        })
+                    emotes[emote_parts[0]] = text_pos.copy()
+                parsed_tags[parsed_tag[0]] = emotes.copy()
+            else:
+                parsed_tags[parsed_tag[0]] = None
+        elif parsed_tag[0] == 'emote-sets':
+            emote_set_ids = tag_value.split(',')
+            parsed_tags[parsed_tag[0]] = emote_set_ids
+        else:
+            if parsed_tag[0] in tags_to_ignore.keys():
+                pass
+            else:
+                parsed_tags[parsed_tag[0]] = tag_value
+    return parsed_tags
+
+
+def parse_command(s: str):
+    result = {}
+    tmp = s.split(' ')
+
+    if tmp[0] in ['JOIN', 'PART', 'NOTICE', 'CLEARCHAT', 'HOSTTARGET', 'PRIVMSG']:
+        result = {'command': tmp[0], 'channel': tmp[1]}
+    elif tmp[0] == 'PING':
+        result = {'command': tmp[0]}
+    elif tmp[0] == 'CAP':
+        result = {'command': tmp[0], 'is_cap_request_enabled': True if tmp[2] == 'ACK' else False}
+    elif tmp[0] == 'GLOBALUSERSTATE':
+        result = {'command': tmp[0]}
+    elif tmp[0] in ['USERSTATE', 'ROOMSTATE']:
+        result = {'command': tmp[0], 'channel': tmp[1]}
+    elif tmp[0] == 'RECONNECT':
+        print("The Twitch IRC server is about to terminate the connection for maintenance.")
+        result = {'command': tmp[0]}
+    elif tmp[0] == '421':
+        print('Unsupported IRC command:', tmp[2])
+        result = None
+    elif tmp[0] == '001':
+        result = {'command': tmp[0], 'channel': tmp[1]}
+    elif tmp[0] in ['002', '003', '004', '353', '366', '372', '375', '376']:
+        print("numeric message:", tmp[0])
+        return None
+    else:
+        print(f"\nUnexpected command: {tmp[0]}\n")
+        return None
+    return result
+
+
+def parse_source(s: str):
+    if s is None or len(s) == 0:
+        return None
+    tmp = s.split('!')
+    if len(tmp) == 2:
+        return {'nick': tmp[0], 'host': tmp[1]}
+    else:
+        return {'nick': None, 'host': tmp[0]}
+
+
+def parse_message(msg: str):
+
+    i = 0
+    raw_tags = None
+    raw_source = None
+    raw_command = None
+    raw_parameters = None
+
+    # search tags
+    if msg[i] == '@':
+        j = msg.index(' ')
+        raw_tags = msg[1:j]
+        i = j + 1
+    
+    # search source
+    if msg[i] == ':':
+        i += 1
+        j = msg.index(' ', i)
+        raw_source = msg[i:j]
+        i = j + 1
+    
+    # search command
+    j = msg.find(':', i)
+    if j == -1:
+        j = len(msg)
+    raw_command = msg[i:j].strip()
+
+    # search parameters
+    if j != len(msg):
+        i = j + 1
+        raw_parameters = msg[i:]
+    
+    tags = None
+    source = None
+    command = None
+    parameters = None
+
+    command = parse_command(raw_command)
+    if raw_tags is not None:
+        tags = parse_tags(raw_tags)
+    source = parse_source(raw_source)
+    parameters = raw_parameters
+
+    return {'tags': tags, 'source': source, 'command': command, 'parameters': parameters}
+
+if __name__ == "__main__":
+    from json import dumps
+    print(dumps(parse_message('@badge-info=;badges=;client-nonce=d8747a27930a536e38d795752611d2ed;color=#FF0000;display-name=WB_Ghost;emotes=;first-msg=0;flags=;id=bb42c555-402d-48f2-82a8-40c23b6fa607;mod=0;returning-chatter=0;room-id=135015290;subscriber=0;tmi-sent-ts=1681925706763;turbo=0;user-id=755589900;user-type= :wb_ghost!wb_ghost@wb_ghost.tmi.twitch.tv PRIVMSG #dimadivan :Как дела хоть?'), indent=4))
